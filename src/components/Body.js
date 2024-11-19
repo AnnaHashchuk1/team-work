f59acdda9ac736e7896166db724b53ddb727b197
Yokay85 <voytynskyyda@gmail.com>
2024-11-19 20:41:55 +0200

    Added domain expert

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/yokay-avatar.png"} alt="Danylo Voitynskyi" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Danylo Voitynskyi</h2>
+          <p style={styles.position}>Domain Expert</p>
         </div>
       </div>
     </main>
