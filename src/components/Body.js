b68456486cc228c442a94904175784efeed0c0b4
atfxgo <kateryna.havryshchuk.pz.2023@lpnu.ua>
2024-11-19 20:47:16 +0200

    Added Project Manager

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/Kate_Havryshchuk.png"} alt="Kate Havryshchuk" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Kate Havryshchuk</h2>
+          <p style={styles.position}>Project Manager</p>
         </div>
       </div>
     </main>
